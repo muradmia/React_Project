a79957fb60becf6960974adb1e082206a26aec23
murad-mia <muradmia901@gmail.com>
2025-04-28 19:19:49 +0600

    react_project

---
+++ project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './Projects/Navbar'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className='p-4 text-gray-900'>Hello world</h1> */}
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ project/src/Projects/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+    const [activelink, setActiveLink] = React.useState('#home')
+
+    const navItem = [
+        { href: '#home', label:'Home' },
+        { href: '#about', label:'About Us' },
+        { href: '#services', label:'Our services' },
+        { href: '#testimonials', label:'Testimonials' },
+        
+    ]
+
+  return (
+    <nav className= "fixed top-0 left-0 right-0 bg-white-/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm">
+        <div className="w-full container mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8 md:h-20 h-26">
+            {/* Logo Section */}
+            <div className='flex items-center gap-1 cursor-pointer'>
+                <div className='w-4 h-4 bg-blue-600 rounded-full opacity-75 hover:opacity-100 transition-opacity'></div>
+                <div className="w-4 h-4 -ml-2 bg-red-600 rounded-full opacity-75 hover:opacity-100 transition-opacity"></div>
+            </div>
+            {/* deskto nav items */}
+            <div className="hidden md:flex items-center gap-4">
+
+                {
+                    navItem.map((item,index) =>(
+                        <a key={index} href={item.href} onClick={()=>setActiveLink(item.href)} className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 hover:after:w-full after:bg-blue-600 after:transition-all ${activelink == item.href ? "text-blue-600 after:w-full" : "text-gray-600 hover:text-gray-900"}`}>{item.label}</a>
+                    ))
+                }
+            </div>
+            {/* get in touch */}
+            <button className="hidden md:block bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-800 text-sm font-medium transition-all hover:shadow-lg hover:shadow-blue-100">
+                <a href="#newsletter">Get in Touch</a>
+            </button>
+
+            {/* Mobile menu */}
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
