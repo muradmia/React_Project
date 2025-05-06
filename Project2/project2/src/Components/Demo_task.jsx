148cdbd9945c35f141a3ffc1e806e8bff89d120e
murad-mia <muradmia901@gmail.com>
2025-05-06 20:34:05 +0600

    react-project:

--- Project2/project2/src/Components/Demo_task.jsx
+++ Project2/project2/src/Components/Demo_task.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Demo_task = () => {
+    const [tasks,setTasks] = useState([])
+    const [newTask,setNewTask] = useState('')
+
+    const add = () =>{
+
+        if(newTask.trim()){
+            setTasks([...tasks,{
+                id:Date.now(),
+                text:newTask,
+                complete:false,
+                createAt:new Date()
+            }]),
+            setNewTask('')
+        }
+    }
+
+    const addTask = () => {
+        if (newTask.trim()) {
+          setTasks([...tasks, {
+            id: Date.now(),
+            text: newTask,
+            completed: false,
+            createdAt: new Date()
+          }]);
+          setNewTask('');
+        }
+      };
+
   return (
     <section className='bg-gray-100 w-full h-screen'>
         <div className='container mx-auto  pt-20 '>
             <div className='max-w-[500px]  mx-auto bg-white rounded-lg shadow py-6 px-10'>
                 <h2 className='text-2xl font-bold '>Task Manager</h2>
                 <div>
-                    <input type='text' placeholder='Add a new task' className='border border-gray-700 py-3 px-12 mt-4 rounded-xl'/>
-                    <button className='bg-blue-500 text-white px-5 py-3 rounded-lg hover:bg-blue-600 transition-colors ml-2 cursor-pointer'>Add</button>
+                    <input type='text' value={newTask} onChange={(e)=>setNewTask(e.target.value)} placeholder='Add a new task' className='border border-gray-700 py-3 px-12 mt-4 rounded-xl'/>
+                    <button onClick={add} className='bg-blue-500 text-white px-5 py-3 rounded-lg hover:bg-blue-600 transition-colors ml-2 cursor-pointer'>Add</button>
                 </div>
                 <div>
                     <button className='bg-blue-500 px-5 mt-6 text-white py-3 rounded-lg me-2 cursor-pointer hover:bg-blue-700 transition:color'>All</button>
@@ -18,9 +46,22 @@ const Demo_task = () => {
                 </div>
 
                 <p className='text-gray-600 mt-4'>0 task remaining</p>
+
+                <div>
+                    {
+                        tasks.map((task,index)=>(
+                            <div key={index} className=' bg-gray-200 py-1 mb-2 px-4 rounded-lg '>
+                                <p className=''>{task.text}</p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
             
         </div>
+
+        {/* Show todo */}
+        
     </section>
   )
 }
